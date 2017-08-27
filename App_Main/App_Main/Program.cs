d3e595b7b4a38d4069e93931834d3acec0c6c654
이원재 <mellowlee@naver.com>
2017-08-26 22:48:15 -0300

    최초 변경 내용 반영

---
+++ App_Main/App_Main/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String inputData = Console.ReadLine();
+
+
+            switch(Convert.ToInt32(inputData))
+            {
+                case 0: break;
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+                case 5: break;
+            }
+
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+### DESIGN PATTERNS IN C#
+
+	해당 문서는 DESIGN PATTERNS을 공부한 것을 실제로 Coding하면서
+	익히는데 목적이 있습니다.
+
+### 참고 서적
+	
+	제목: JAVA 언어로 배우는 디자인 패턴 입문
+	
+	![](http://image.kyobobook.co.kr/images/book/xlarge/914/x9788931436914.jpg)
+	
